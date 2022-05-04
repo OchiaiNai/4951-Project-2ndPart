79b11aa7b4eda05734bd4a3d2333431055669a6c
LiliumJadez <ranserica@outlook.com>
2022-05-05 07:37:00 +0800

    feedbackModify
    Reshape mechanism and performance of feedback function

--- WeChatMiniProgram/miniprogram/pages/index/workspace/feedback/feedback.js
+++ WeChatMiniProgram/miniprogram/pages/index/workspace/feedback/feedback.js
@@ -1,70 +1,126 @@
-// pages/index/workspace/feedback/feedback.js
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
-        message:""
+      step: 1,
+      type: 0,
+      title: '',
+      hint: '',
+      color: '',
+      message: '',
+      contact: '',
+      submitText: 'Submit'
     },
-    submit: function(){
-        wx.switchTab({
-          url: '../../workspace/workspace',
+  
+    handleType1() {
+      this.setData({
+        step: 2,
+        type: 1,
+        title: 'Feedback',
+        hint: 'Please enter your feedback or questions',
+        color: '#42da56',
+        message: '',
+        contact: '',
+        submitText: 'Submit'
+      })
+    },
+  
+    handleType2() {
+      this.setData({
+        step: 2,
+        type: 2,
+        title: 'Program Debug',
+        hint: 'Please provide us the error you meet',
+        color: '#3268cf',
+        message: '',
+        contact: '',
+        submitText: 'Submit'
+      })
+    },
+  
+    handleMessageChange(e) {
+      this.setData({
+        message: e.detail.value
+      })
+    },
+  
+    handleContactChange(e) {
+      this.setData({
+        contact: e.detail.value
+      })
+    },
+  
+    // 提交前先校验数据
+    handleSubmit() {
+      if (this.data.message.replace(/\s+/g, '') == '') {
+        wx.showToast({
+          title: 'Please enter your feedback',
+          icon: 'loading',
+          duration: 1500
         })
+        return;
+      }
+      if (this.data.message.length > 100) {
+        wx.showToast({
+          title: 'Contents too long',
+          icon: 'loading',
+          duration: 1500
+        })
+        return;
+      }
+      if (this.data.contact.length > 50) {
+        wx.showToast({
+          title: 'Contact too long',
+          icon: 'loading',
+          duration: 1500
+        })
+        return;
+      }
+      this.setData({
+        submitText: 'Processing..'
+      })
+      wx.request({
+        url: 'http://www.xxx.cn/InsertFeedback.ashx',
+        data: {
+          'Type': this.data.type,
+          'Message': this.data.message,
+          'Contacts': this.data.contact
+        },
+        method: "POST",
+        header: {
+          'content-type': 'application/x-www-form-urlencoded'
+        },
+        success: this.submitSuccess.bind(this),
+        fail: this.submitFail.bind(this)
+      })
     },
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
-
+  
+    submitSuccess(res){
+      var r = res.data;
+      if(r == "ok") {
+        this.setData({
+          step: 3
+        })
+      }
+      else {
+        wx.showToast({
+          title: 'Submission Fail',
+          icon: 'loading',
+          duration: 1800
+        })
+      }
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
-
+  
+    submitFail() {
+      wx.showToast({
+        title: 'Submission Fail',
+        icon: 'loading',
+        duration: 1800
+      })
     },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
+  
+    handleBack(){
+      this.setData({
+        step: 1
+      })
     }
-})
+  })
+  
